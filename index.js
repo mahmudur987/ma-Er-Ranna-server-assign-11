34657cd0c9863b767968e4d4ac57b8b2e584641b
mahmud987 <safemahmud987@gmail.com>
2022-11-09 01:29:59 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require('express')
+const app = express();
+const cors = require('cors');
+require('dotenv').config()
+
+const port = process.env.PORT
+// middlewere
+app.use(cors());
+app.use(express.json());
+
+// mongodb Connection
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.ddhlldi.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+// Mongodb CURD operation
+async function run() {
+    try {
+        const database = client.db('MaErRanna');
+        const dishesCollection = database.collection('dishes');
+        const reviewsCollection = database.collection('reviews');
+        const feturesCollection = database.collection('features');
+        app.get('/dishes', async (req, res) => {
+            const query = {};
+            const dishes = await dishesCollection.find(query).limit(3).toArray();
+            res.send(dishes)
+        });
+        app.get('/alldishes', async (req, res) => {
+            const query = {};
+            const dishes = await dishesCollection.find(query).toArray();
+            res.send(dishes)
+        });
+        app.get('/dishes/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const dish = await dishesCollection.findOne(query);
+            res.send(dish)
+        });
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewsCollection.insertOne(review);
+            res.send(result)
+        });
+        app.get('/reviews', async (req, res) => {
+            const foodName = req.query.foodname;
+            const filter = { foodname: foodName };
+            const reviews = await reviewsCollection.find(filter).toArray();
+            res.send(reviews)
+        });
+        app.get('/features', async (req, res) => {
+            const query = {};
+            const features = await feturesCollection.find(query).toArray();
+            res.send(features)
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+const services = require('./service.json')
+
+app.get('/services', (req, res) => {
+    res.send(services)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
